9da751b21524cc9ab5f116d67921a0a5841fe96c
Alfonso Vásquez <avasquez@rivetlogic.com>
2015-09-23 20:01:49 -0600

    * Fix for CRAFTERCMS-1468. * Refactored SearchUpdateProcessor and SearchUpdateFlattenXmlProcessor. * Added utility processors: OnPathMatchConditionalProcessor and PerSiteConditionalProcessor.

---
+++ cstudio-publishing-receiver/src/main/java/org/craftercms/cstudio/publishing/processor/PerSiteConditionalProcessor.java
@@ -0,0 +1,7 @@
+package org.craftercms.cstudio.publishing.processor;
+
+/**
+ * Created by alfonsovasquez on 23/9/15.
+ */
+public class PerSiteConditionalProcessor {
+}
---
+++ cstudio-publishing-receiver/src/main/java/org/craftercms/cstudio/publishing/utils/XmlUtils.java
@@ -0,0 +1,7 @@
+package org.craftercms.cstudio.publishing.utils;
+
+/**
+ * Created by alfonsovasquez on 21/9/15.
+ */
+public class XmlUtils {
+}
